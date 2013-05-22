a56fd2c17ad0e362bb07d8b4909b1d8a6eb38b96
simplay <michael.single@students.unibe.ch>
2013-05-22 15:05:17 +0200

    added one array core extension test - output should look like of valid input.

--- spec/lib/core_extensions/array_spec.rb
+++ spec/lib/core_extensions/array_spec.rb
@@ -14,9 +14,20 @@ describe Array do
         to raise_error ArgumentError
     end
     
-    it "should return an empty Array if subject is an empty Array" do
+    it "should return an empty Array if subject is an empty" do
       subject.levenshtein_suggestions(Object.new).
         should be_empty
+    end
+    
+    it "should return an Array of String if given Argument is a String and subject is not empty" do
+      word_list = Array.new
+      rand(1000).times { word_list.push(String.new) }
+      suggestions = word_list.levenshtein_suggestions(String.new)
+      suggestions.should be_a Array 
+      
+      suggestions.each do |suggestion|
+        suggestion.should be_a String 
+      end 
     end
   
   end
