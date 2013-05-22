dc0a218e8d11ef16969fcf8042a41fff35d19e55
simplay <michael.single@students.unibe.ch>
2013-05-22 14:44:34 +0200

    added two array core extension tests for levenstein suggestions what should be returned for edge cases

--- spec/lib/core_extensions/array_spec.rb
+++ spec/lib/core_extensions/array_spec.rb
@@ -8,6 +8,16 @@ describe Array do
     it "should exist" do
       subject.should respond_to :levenshtein_suggestions
     end
+    
+    it "should raise an ArgumentError unless given Argument is a String if subject is an empty Array" do
+      expect { [String.new].levenshtein_suggestions(Object.new) }.
+        to raise_error ArgumentError
+    end
+    
+    it "should return an empty Array if subject is an empty Array" do
+      subject.levenshtein_suggestions(Object.new).
+        should be_empty
+    end
   
   end
 end
